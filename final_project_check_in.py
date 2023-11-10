3c51aeabf6db814711d2236580ed229f31663ff0
Tiara Eltrevoog <tiaraeltrevoog@tiaras-mbp.mynetworksettings.com>
2023-11-09 19:56:21 -0500

    added human_player_move() method

--- final_project_check_in.py
+++ final_project_check_in.py
@@ -12,3 +12,23 @@ def winner(self, playerHealth, computerHealth):
     if computerHealth == None:
         if playerHealth == True:
             return "Player won!"
+        
+def human_player_move(self, data):
+    """Asks the human player to make their next move.
+
+    Args:
+        data (list of str): game progress.
+
+    Raises:
+        ValueError: player's input does not match an item in the move_options 
+            list.
+
+    Returns:
+        move (str): the player's move.
+    """
+    self.data = data
+    move = input(f"{self.name}, select your next move: {self.move_options}")
+    if move in self.move_options:
+        return move
+    else:
+        raise ValueError("you must select a valid move")
