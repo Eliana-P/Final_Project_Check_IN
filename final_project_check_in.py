2c25c9cafd9bb8a0ee10e0792853b8037f88824c
nithyaruna <143642443+nithyaruna@users.noreply.github.com>
2023-12-03 14:41:20 -0500

    Update HorrorGame class and methods
    some blank methods anyone can create

--- final_project_check_in.py
+++ final_project_check_in.py
@@ -55,7 +55,6 @@ def computerHealth(damage, healthstatus):
 
 def winner(playerHealth, computerHealth):
     """Returns the winner of the game based on health.
-    
     Returns: 
         Returns a string stating that computer won. 
         Returns a string stating that player won. 
@@ -78,41 +77,84 @@ def monster():
 
 
 class HumanPlayer:
-    def __init__(self, name):
+    def __init__(self, name, choice):
         self.name = name
-        
+        self.choice = choice
         
     def move(self, data):
         """Asks the human player to make their next move.
-
         Args:
             data (list of str): tracks game progress.
-
         Raises:
             ValueError: player's input does not match an item in the move_options 
                 list.
-
         Returns:
             move (str): the player's move.
         """
         self.data = data
         print(data)
-        human_player_move = input(f"Will you {self.option1} or {self.option2}?")
+        human_player_move = input(f"{self.name}, select a move: {self.choice}")
         
         if human_player_move in self.options:
             return human_player_move
         else:
             raise ValueError("you must select a valid move")
 
-
 class ComputerPlayer (HumanPlayer):   
     def move(self, data):
         self.data = data
         computer_player_move = random.choice(self.options)
         return computer_player_move
+        # VS Code tells me this is unreachable, I don't know if that's true but can someone check
         super().move(data)
 
-class SawGame():
+class HorrorGame():
     def __init__(self, human_name, computer_name):
-        self.human_player = 
-        self.computer_player = 
+        self.human_player = HumanPlayer(human_name, ["attack", "defend yourself"])
+        self.computer_player = ComputerPlayer(computer_name, ["attack", "defend yourself"])
+        self.level = 1
+        self.human_health = 100
+        self.computer_health = 100
+        self.game_over = False
+    
+    def run(self):
+        #I'm going to leave basic print statements for the text, narration part of the game
+        #Can someone please change these to match the story lore
+
+        print("Welcome to this Survival Game")
+        print("You wake up in a dark, locked room...")
+
+        while not self.game_over:
+            if self.level == 1:
+                self.level_1()
+            if self.level == 2:
+                self.level_2()
+        
+        self.the_winner()
+    #this needs to be written
+    def level_1(self):
+        pass
+
+    #make sure to incorporate the monster method if the user makes it to level2
+    def level_2(self):
+        pass
+
+    #if the human attacks computer, print computer health, if the human defends, print that we defended
+    def human_move(self):
+        pass
+
+    #if the computer attacks human, print human health and if the computer defends, print that it defended
+    def computer_move():
+        pass
+
+    def the_winner(self):
+        who_won = winner(self.human_health, self.computer_health)
+        if who_won:
+            print(who_won)
+
+if __name__ == "__main__":
+    #change this to argparse and import ArgumentParser so the user can input player and computer name
+    #for now, this is a placeholder
+    game = Game("User", "Computer")
+    game.run()
+    game = HorrorGame("")
