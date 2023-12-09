633c79061b17fc05d233d5542a4284b38bd5be52
Eliana-P <143539776+Eliana-P@users.noreply.github.com>
2023-12-09 16:17:29 -0500

    Update final_project_check_in.py

--- final_project_check_in.py
+++ final_project_check_in.py
@@ -8,19 +8,22 @@ class HumanPlayer:
         name (str): player's name.
         options (list of str): the player's move options.
         health (int): player's current health.
+        move (str): player's current move
     """
-    def __init__(self, name, options):
+    def __init__(self, name, options, move):
         """Populates attributes.
 
         Args:
             name (str): player's name.
             options (list of str): player's move options.
+            move (str): player's move.
             
         Side effects:
             Populates attributes.
         """
         self.name = name
         self.options = options
+        self.move = move
         self.health = 100
     
     def __str__(self):
@@ -29,25 +32,28 @@ class HumanPlayer:
         """
         return (f"\t name: {self.name}\n\t health: {self.health}")  #magic method
         
-    def move(self):
+    def make_move(self):
         """Asks the human player to make their next move.
         
         Raises:
             
         Returns:
             player input (str): player's input to select their move. 
+        
+         Side effects:
+            print (str): prints a string saying the user has exited the game.
+            print (str): prints string telling the user it was a invalid input and to choose a new move.
         """
-        while True:
-            user_input = input(f"\n{self.name}, select your next move: {self.options}: ")
+        user_input = input(f"\n{self.name}, select your next move: {self.options}: ")
             
-            if user_input.lower() == "exit":
-                print("Exiting the game.")
-                exit()
+        if user_input.lower() == "exit":
+            print("Exiting the game.")
+            exit()
 
-            if user_input in self.options:
-                return user_input
-            else:
-                print("Invalid input - Please choose a valid move!")
+        if user_input in self.options:
+            self.move = user_input
+        else:
+            print("Invalid input - Please choose a valid move!")
 
 class ComputerPlayer(HumanPlayer):
     """Subclass of HumanPlayer class. Represents a computer player.
@@ -57,19 +63,20 @@ class ComputerPlayer(HumanPlayer):
         options (list of str): the player's move options.
         health (int): player's current health.
     """
-    def __init__(self, name, options):
+    def __init__(self, name, options, move):
         """Populates attributes.
         
         Args:
             name (str): the player's name name.
             options (list of str): the player's move options.
+            move (str): player's move.
         """
-        super().__init__(name, options) # this super should work
+        super().__init__(name, options, move) # this super should work
         
 class HorrorGame:
     def __init__(self, human_name, computer_name):
-        self.human_player = HumanPlayer(name=human_name, options = ["attack", "defend", "tape"])  #keyword argument
-        self.computer_player = ComputerPlayer(name = computer_name, options= ["attack", "defend", "tape"])
+        self.human_player = HumanPlayer(name=human_name, move="", options = ["attack", "defend", "tape"])  #keyword argument
+        self.computer_player = ComputerPlayer(name = computer_name, move="", options= ["attack", "defend", "tape"])
         self.level = 1
         self.game_over = False  # controls game loop
         self.advanced_to_level_2 = False #controls level 2 lol
@@ -87,10 +94,11 @@ class HorrorGame:
         while not self.game_over:
             if self.level == 1:
                 # check if the level 1 is completed
-                if self.level_1():
-                    break
-                if not self.game_over and self.level == 2:
-                    self.level_2()
+                self.level_1()
+            if self.level == 2:
+                self.level_2()
+                    
+                
                 '''
                 narration2 = input("Would you like to read the narration before the game commences? [y/n]: ")
                 if narration2.lower() == "y":
@@ -98,7 +106,8 @@ class HorrorGame:
                         for line in f:
                             print(line.strip())
                 '''
-        if not self.advanced_to_level_2:
+        
+        if self.level != 2:
             play_again = input("Would you like to play again? [y/n]: ")
             if play_again.lower() == "y" or play_again.lower() == "yes":
                 self.reset_game()
@@ -110,38 +119,32 @@ class HorrorGame:
             self.run()
         '''
     def level_1(self):
-        human_move = self.human_player.move()
+        self.human_player.make_move()
         computer_move = random.choice(self.computer_player.options)
 
-        self.handle_move(self.human_player, human_move, self.computer_player, computer_move)
+        self.handle_move(self.human_player, self.computer_player, computer_move)
 
-        if self.winner() == f"{self.human_player.name}" and self.level == 1:
+        if self.winner() and self.level == 1:
             self.level += 1
             self.human_player.health = 100
             self.computer_player.health = 100
-            print(f"{self.human_player.name} found the tape recorder and advances to Level 2!")
             self.advanced_to_level_2 = True
 
             # ends the function and breaks out of the loop
+            pighead_name = "Pighead"
+            pighead = ComputerPlayer(name = pighead_name, options = ["attack", "defend", "tape"], move="")
+            self.computer_player = pighead
             return True
-
-        # computer's move only if the game is not over
-        if not self.game_over and self.level == 2:
-            self.level_2()
-
+            
         # return False if the game is not over or advanced to level 2
         return False
 
     def level_2(self):
-        human_move = self.human_player.move()
+        self.human_player.make_move()
         monster_move = random.choice(["attack", "defend", "tape"])
 
-        pighead_name = "Pighead"
-        pighead = ComputerPlayer(name = pighead_name, options = ["attack", "defend", "tape"])
-
-        self.handle_move(self.human_player, human_move, pighead, monster_move)
-        #self.handle_move(self.computer_player, monster_move, self.human_player, human_move)
-
+        self.handle_move(self.human_player, self.computer_player, monster_move)
+        
         self.game_over = True if self.winner() else False
 
     def winner(self):
@@ -151,20 +154,18 @@ class HorrorGame:
             print(str(self.computer_player))
             self.game_over = True
             return True
-
-        elif self.human_player.options[-1] == "tape" and random.random() < 0.10:
-            print(f"{self.human_player.name} found the tape recorder and advances to Level 2!")
-            self.level += 1
-            self.computer_player.health = 100
-
+        
         elif self.computer_player.health <= 0:
-            print(f"Congratulations! {self.human_player.name} wins the game!")
+            print(f"Congratulations! {self.human_player.name} wins the game! Advancing to level {self.level + 1}!")
             print("Winners stats:")
             print(str(self.human_player))  # human player stats
-            self.game_over = True
             return True
 
-        elif self.computer_player.options[-1] == "tape" and random.random() < 0.05:
+        elif self.human_player.move == "tape" and random.random() < 0.10:
+            print(f"{self.human_player.name} found the tape recorder and advances to Level {self.level + 1}!")
+            return True
+
+        elif self.computer_player.move == "tape" and random.random() < 0.05:
             print(f"{self.computer_player.name} found the tape recorder! {self.human_player.name} loses!")
             print("Winners stats:")
             print(str(self.computer_player))
@@ -173,10 +174,10 @@ class HorrorGame:
 
         return False
 
-    def handle_move(self, attacker, attacker_move, defender, defender_move):
+    def handle_move(self, attacker, defender, defender_move):
         damage = random.randint(10, 20)
 
-        if attacker_move == "attack":
+        if attacker.move == "attack":
             print(f"\n{attacker.name} attacked!")
             if defender_move == "defend":
                 print(f"{defender.name} defended!")
@@ -187,7 +188,7 @@ class HorrorGame:
                 print(f"{defender.name} attacked!")
                 self.human_player.health -= damage
                 self.computer_player.health -= damage
-        elif attacker_move == "defend":
+        elif attacker.move == "defend":
             print(f"{attacker.name} defended!")
             if defender_move == "attack":
                 print(f"{defender.name} attacked!")
@@ -195,7 +196,7 @@ class HorrorGame:
                 print(f"{defender.name} was looking for the tape!")
             elif defender_move == "defend":
                 print(f"{defender.name} defended!")
-        elif attacker_move == "tape":
+        elif attacker.move == "tape":
             print(f"{attacker.name} was looking for the tape!")
             if defender_move == "attack":
                 print(f"{defender.name} attacked!")
